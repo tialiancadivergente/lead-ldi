e00a6b4d43aa84188431698a5422c7a446b4dcd9
Felipe Sousa <felipepedrosousa@hotmail.com>
2026-03-24 00:07:13 -0300

    Update WhatsApp URLs in quest-config for temperature keys

--- lib/config/quest-config.ts
+++ lib/config/quest-config.ts
@@ -2,10 +2,11 @@ export const DEFAULT_QUEST_FORM_VERSION_ID =
   "2228a3ba-b79e-41b0-90a0-47cac5816209";
 
 const QUEST_WHATSAPP_URL_BY_TEMPERATURE = {
-  f: "https://redirects.aliancadivergente.com.br/ldi ",
-  org: "https://redirects.aliancadivergente.com.br/ldi ",
-  m: "https://redirects.aliancadivergente.com.br/ldi ",
-  q: "https://redirects.aliancadivergente.com.br/ldi ",
+  f: "https://redirects.aliancadivergente.com.br/ldi",
+  org: "https://sendflow.click/i/ldi-org",
+  o: "https://sendflow.click/i/ldi-org",
+  m: "https://redirects.aliancadivergente.com.br/ldi",
+  q: "https://redirects.aliancadivergente.com.br/ldi",
 } as const;
 
 type QuestTesteTemperatureKey = keyof typeof QUEST_WHATSAPP_URL_BY_TEMPERATURE;
